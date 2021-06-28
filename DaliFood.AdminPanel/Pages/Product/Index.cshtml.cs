df474b3d1ce90c82aa1bfa7d7d4aa95e81349090
Mahdi20062020 <hoseinpoor2006@Gmail.com>
2021-06-28 18:02:29 +0430

    Build Product Pages

--- DaliFood.AdminPanel/Pages/Product/Index.cshtml.cs
+++ DaliFood.AdminPanel/Pages/Product/Index.cshtml.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DaliFood.Utilites;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -9,8 +10,27 @@ namespace DaliFood.AdminPanel.Pages.Product
 {
     public class IndexModel : PageModel
     {
+        readonly UnitOfWork unitofwork;
+        public IndexModel(UnitOfWork unitofwork)
+        {
+            this.unitofwork = unitofwork;
+        }
+        [BindProperty]
+        public IEnumerable<Models.Product> Product { get; set; }
+
         public void OnGet()
         {
+            Product = unitofwork.ProductRepository.GetAll(orderby: p => p.OrderByDescending(p => p.CreateDate));
+        }
+        public ActionResult OnGetDelete(int Id)
+        {
+            var Product = unitofwork.ProductRepository.GetById(Id);
+            if (Product == null)
+                return NotFound();
+            unitofwork.ProductRepository.Delete(Product);
+            unitofwork.ProductRepository.Save();
+            return RedirectToPage("Index");
+
         }
     }
 }
--- DaliFood.AdminPanel/Pages/Product/Upsert.cshtml.cs
+++ DaliFood.AdminPanel/Pages/Product/Upsert.cshtml.cs
@@ -2,15 +2,61 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using DaliFood.Utilites;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace DaliFood.AdminPanel.Pages.Product
 {
     public class UpsertModel : PageModel
     {
-        public void OnGet()
+
+        readonly UnitOfWork unitofwork;
+        public UpsertModel(UnitOfWork unitofwork)
         {
+            this.unitofwork = unitofwork;
+        }
+        [BindProperty]
+        public Models.Product Product { get; set; }
+
+        public ActionResult OnGet(int? Id)
+        {
+            Product = new Models.Product();
+            if (Id == null)
+            {
+                ViewData["CategorieId"] = new SelectList(unitofwork.ProductCategorieRepository.GetAll(),"Id", "Name");
+                ViewData["RestaurantId"] = new SelectList(unitofwork.RestaurantRepository.GetAll(),"Id", "Name");          
+                return Page();
+            }
+
+            Product = unitofwork.ProductRepository.GetById(Id);
+
+            if (Product == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategorieId"] = new SelectList(unitofwork.ProductCategorieRepository.GetAll(), "Id", "Name", Product.CategorieId);
+            ViewData["RestaurantId"] = new SelectList(unitofwork.RestaurantRepository.GetAll(), "Id", "Name",Product.RestaurantId);
+            return Page();
+        }
+        public ActionResult OnPost()
+        {
+            if (Product.Id == 0)
+                Product.CreateDate = DateTime.Now;
+            if (!ModelState.IsValid)
+                return Page();
+
+            if (Product.Id == 0)
+            {
+                unitofwork.ProductRepository.Create(Product);
+            }
+            else
+            {
+                unitofwork.ProductRepository.Modifie(Product);
+            }
+            unitofwork.ProductRepository.Save();
+            return RedirectToPage("Index");
         }
     }
 }
