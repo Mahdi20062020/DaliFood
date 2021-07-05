8cf8f1e4d701a82f6324439e56573a8d43beb105
Mahdi20062020 <hoseinpoor2006@Gmail.com>
2021-07-05 22:38:55 +0430

    add CustomerProduct Apicontroller

---
+++ DaliFood.WebApi/Controllers/CustomerProductController.cs
@@ -0,0 +1,64 @@
+﻿using DaliFood.Utilites;
+using DaliFood.WebApi.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaliFood.WebApi.Controllers
+{
+    [Route("/Api")]
+    [ApiController]
+    public class CustomerProductController : Controller
+    {
+        readonly UnitOfWork unitofwork;
+        public CustomerProductController(UnitOfWork unitofwork)
+        {
+            this.unitofwork = unitofwork;
+        }
+        [HttpGet]
+        public IActionResult GetCustomerProducts(int ItemPerPage, int PageNum, int? TypeId)
+        {
+            var Items = unitofwork.CustomersProductRepository.GetAll(orderby: p => p.OrderByDescending(p => p.CreateDate));
+            List<CustomersProduct> ItemsforShow = new List<CustomersProduct>();
+            foreach (var item in Items)
+            {
+                item.Product = unitofwork.ProductRepository.GetById(item.ProductId);
+                item.Product.ProductCategorie = unitofwork.ProductCategorieRepository.GetById(item.Product.CategorieId);
+                item.Customer = unitofwork.CustomerRepository.GetById(item.CustomerId);
+
+                ItemsforShow.Add(item);
+            }
+            IEnumerable<CustomersProduct> result = ItemsforShow;
+            if (TypeId.HasValue)
+            {
+                result = ItemsforShow.Where(p => p.ProductTypeId == TypeId);
+            }
+            var Skipcount = (PageNum - 1) * ItemPerPage;
+            result = result.Skip(Skipcount);
+            result = result.Take(ItemPerPage);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+        [HttpGet("CustomerProducts/{Id}")]
+        public IActionResult GetCustomerProduct(int Id)
+        {
+            var item = unitofwork.CustomersProductRepository.GetById(Id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            CustomersProduct ItemforShow;
+            item.Product = unitofwork.ProductRepository.GetById(item.ProductId);
+            item.Product.ProductCategorie = unitofwork.ProductCategorieRepository.GetById(item.Product.CategorieId);
+            item.Customer = unitofwork.CustomerRepository.GetById(item.CustomerId);
+            ItemforShow = item;
+            return Ok(ItemforShow);
+        }
+    }
+}
---
+++ DaliFood.WebApi/ViewModels/CustomersProduct.cs
@@ -0,0 +1,39 @@
+﻿using DaliFood.Models;
+using DaliFood.Utilites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DaliFood.WebApi.ViewModels
+{
+    public class CustomersProduct
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public string ProductType { get; set; }
+        public int ProductTypeId { get; set; }
+        public string Description { get; set; }
+        public string CustomerName { get; set; }
+        public int Price { get; set; }
+        public int PriceWithDiscount { get; set; }
+
+        public static implicit operator CustomersProduct(Models.CustomersProduct _customersproduct) 
+        {
+            CustomersProduct customersProduct = new();
+            customersProduct.Id = _customersproduct.Id;
+            customersProduct.ProductName = _customersproduct.Product.Name;
+            customersProduct.ProductType = _customersproduct.Product.ProductCategorie.Name;
+            customersProduct.ProductTypeId = _customersproduct.Product.CategorieId;
+            customersProduct.Price = _customersproduct.Price;
+            customersProduct.CustomerName = _customersproduct.Customer.Name;
+            customersProduct.Description = _customersproduct.Description;
+            if (_customersproduct.Discount!=null)
+            {
+                customersProduct.PriceWithDiscount = customersProduct.Price.UseDiscount(_customersproduct.Discount.DiscountRate);
+            }
+            return customersProduct;
+        }
+      
+    }
+}
